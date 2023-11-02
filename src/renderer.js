ad2da792258de9b6476142264de858781093b523
dbutelle <butelledylan@gmail.com>
2023-11-02 12:15:06 +0100

    Fixing notifications spamming.

--- src/renderer.js
+++ src/renderer.js
@@ -10,6 +10,7 @@ const temperatureChart = createChart(cpu);
 const input = document.getElementsByTagName('input')[0];
 const help = document.getElementById('open-notifications');
 let value = input.value;
+let wasHot = false;
 
 input.addEventListener('keyup', () => {
   value = input.value
@@ -34,9 +35,15 @@ const addDataPoint = async () => {
       temperatureChart.data.datasets[0].data.shift();
     }
 
-    if (response.main > value) {
+    let isHot = response.main > value;
+
+    if (isHot && !wasHot) {
       await window.versions.showNotification('Température élevée', `La température du CPU est de plus de ${value}°C.`);
+      wasHot = true;  // Met à jour l'état précédent de isHot
+    } else if (!isHot) {
+      wasHot = false;  // Met à jour l'état précédent de isHot lorsque isHot repasse en dessous de value
     }
+
   } catch (error) {
     console.error(error);
     weather.innerText = 'Erreur lors de la récupération de la température CPU';
